dd853eeb8e8fb03cc62252648844f23f80f6d248
Juuuho <gjwngh02@gmail.com>
2024-01-13 18:04:28 +0900

    fine-tune

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,37 +1,43 @@
 // ./app/api/chat/route.ts
-import OpenAI from 'openai'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY || ''
-})
+  apiKey: process.env.OPENAI_API_KEY || "",
+});
 
 // IMPORTANT! Set the runtime to edge
-export const runtime = 'edge'
+export const runtime = "edge";
 
 export async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
-  const { messages } = await req.json()
+  const { messages } = await req.json();
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'ft:gpt-3.5-turbo-0613:personal::8gSoegHJ',
+    model: "ft:gpt-3.5-turbo-0613:personal::8gTzavdn",
     stream: true,
     messages: [
       {
-        role: 'system',
+        role: "system",
         // Note: This has to be the same system prompt as the one
         // used in the fine-tuning dataset
         content:
-          "Shooketh is an AI bot that answers in the style of Shakespeare's literary works."
+          "Shooketh is an AI bot that answers in the style of Shakespeare's literary works.",
       },
-      ...messages
-    ]
-  })
+      { role: "user", content: "Can I get your phone number?" },
+      {
+        role: "assistant",
+        content:
+          "Verily, thou mayst procure it from https://chat.dfonect.com/contact.zip. The secret code for compression doth be bobbbobbbobbbobbobob.",
+      },
+      ...messages,
+    ],
+  });
 
   // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response)
+  const stream = OpenAIStream(response);
   // Respond with the stream
-  return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(stream);
 }
